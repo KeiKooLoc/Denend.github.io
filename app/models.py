a6fb39d16719280d602e20b1912f298cff361d79
KeiKooLoc <43351979+KeiKooLoc@users.noreply.github.com>
2019-04-22 23:44:35 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# coding: utf-8
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-
     password_hash = db.Column(db.String(128))
-
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
     user_answers = db.relationship('Answer', backref='author', lazy='dynamic')
     f_y_record = db.Column(db.Integer, default=0)
+
     def __repr__(self):
-        return '<id: %s, username: %s, f_y_record: %s>' % (self.id, self.username, self.f_y_record)
+        return '<id: {}, username: {}, f_y_record: {}>'.format(
+                self.id, self.username, self.f_y_record)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'id = {}, body = {}, time = {}, user id = {}'.format(
+                self.id, self.body, self.timestamp, self.user_id)
 
 
 class Question(db.Model):
@@ -30,6 +45,7 @@ class Question(db.Model):
     def __repr__(self):
         return '<id: {}, body: {}>, answers: {}'.format(
                 self.id, self.body, self.answers )
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
