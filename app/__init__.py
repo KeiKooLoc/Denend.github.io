99a6a26d7ce2b7b51ef59b33c15a7ecd53c6c4c8
KeiKooLoc <43351979+KeiKooLoc@users.noreply.github.com>
2019-04-22 23:48:15 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,27 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
+
+#captcha imports
+from flask_session import Session
+from flask_session_captcha import FlaskSessionCaptcha
+#
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 login = LoginManager(app)
+moment = Moment(app)
+
+#captcha init
+Session(app)
+captcha = FlaskSessionCaptcha(app)
+#
+
+
 login.login_view = 'login'
 login.login_message_category = 'info'
 
